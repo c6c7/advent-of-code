ed9381f2111b80aa2b9974c2b7f4ed2564f05979
Charles Celerier <charles@chckyn.org>
2021-12-05 14:35:11 -0800

    [2021] Solved Day 3 Part 2

--- src/year2021/day03/mod.rs
+++ src/year2021/day03/mod.rs
@@ -26,10 +26,14 @@ fn calc_epsilon_rate(gamma_rate: u16) -> u16 {
     !gamma_rate & !(u16::MAX << NUM_BINARY_DIGITS)
 }
 
-pub fn part1(input: String) {
-    let input = input
+fn split_whitespace_and_convert_binary<'a>(input: &'a String) -> impl Iterator<Item = u16> + 'a {
+    input
         .split_whitespace()
-        .map(|s| u16::from_str_radix(s, 2).expect("Could not convert binary to i64."));
+        .map(|s| u16::from_str_radix(s, 2).expect("Could not convert binary to i64."))
+}
+
+pub fn part1(input: String) {
+    let input = split_whitespace_and_convert_binary(&input);
     let gamma_rate = calc_gamma_rate(input);
     let epsilon_rate = calc_epsilon_rate(gamma_rate);
 
@@ -38,5 +42,43 @@ pub fn part1(input: String) {
     println!(
         "Puzzle Answer: {}",
         (epsilon_rate as u64) * (gamma_rate as u64)
+    );
+}
+
+fn filter_by_preferred<F>(input: &String, preferred_fn: F) -> u16
+where
+    F: Fn(i64) -> u16,
+{
+    let mut remaining: Vec<u16> = split_whitespace_and_convert_binary(&input).collect();
+    for i in 0..NUM_BINARY_DIGITS {
+        let shift = NUM_BINARY_DIGITS - 1 - i;
+        let preferred = preferred_fn(remaining.iter().fold(0, |acc, n| {
+            if *n & (1 << shift) > 0 {
+                acc + 1
+            } else {
+                acc - 1
+            }
+        }));
+        assert!(preferred == 0 || preferred == 1);
+        remaining = remaining
+            .into_iter()
+            .filter(|n| (*n & (1 << shift)) >> shift == preferred)
+            .collect();
+        if remaining.len() == 1 {
+            break;
+        }
+    }
+    remaining[0]
+}
+
+pub fn part2(input: String) {
+    let oxygen_generator_rating = filter_by_preferred(&input, |acc| if acc >= 0 { 1 } else { 0 });
+    let co2_scrubber_rating = filter_by_preferred(&input, |acc| if acc >= 0 { 0 } else { 1 });
+
+    println!("Oxygen Generator Rating: {}", oxygen_generator_rating);
+    println!("CO2 Scrubber Rating: {}", co2_scrubber_rating);
+    println!(
+        "Life Support Rating: {}",
+        (oxygen_generator_rating as u64) * (co2_scrubber_rating as u64)
     );
 }
