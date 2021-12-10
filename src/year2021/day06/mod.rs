8c7709b35e85b219832d7680a7c45e818b047b53
Charles Celerier <charles@chckyn.org>
2021-12-09 22:33:36 -0800

    [2021] Solved Day 6 Part 2
    So glad the second part was a simple extension of the first part.

--- src/year2021/day06/mod.rs
+++ src/year2021/day06/mod.rs
@@ -1,10 +1,19 @@
 const NEW_LANTERNFISH_INTERNAL_TIMER: usize = 9;
-const SIMULATION_LENGTH: usize = 80; // days
+const PART_1_SIMULATION_LENGTH: usize = 80; // days
+const PART_2_SIMULATION_LENGTH: usize = 256; // days
 
 pub fn part1(input: String) {
+    simulate_lanternfish(&input, PART_1_SIMULATION_LENGTH);
+}
+
+pub fn part2(input: String) {
+    simulate_lanternfish(&input, PART_2_SIMULATION_LENGTH);
+}
+
+fn simulate_lanternfish(input: &str, simulation_length: usize) {
     let mut lanternfish = parse_lanternfish(&input);
     let mut zero = 0;
-    for _ in 0..SIMULATION_LENGTH {
+    for _ in 0..simulation_length {
         let new_lanternfish = lanternfish[zero];
         zero = (zero + 1) % NEW_LANTERNFISH_INTERNAL_TIMER;
         lanternfish
@@ -14,7 +23,7 @@ pub fn part1(input: String) {
     println!("lanternfish: {:?}", lanternfish);
     println!(
         "after {} days: {}",
-        SIMULATION_LENGTH,
+        simulation_length,
         lanternfish.iter().fold(0, |acc, f| acc + f)
     );
 }
