051f0d9a14ab444a478e82954a135e39ce40e859
Charles Celerier <charles@chckyn.org>
2021-12-11 12:47:44 -0800

    [2021] Extend Day 9 Part 1 code to prepare for Part 2

--- src/year2021/day09/mod.rs
+++ src/year2021/day09/mod.rs
@@ -9,14 +9,29 @@ pub fn part1(input: String) {
     println!("Answer: {}", low_point_risk_level_sum);
 }
 
-struct Heightmap(Vec<Vec<u32>>);
+#[derive(Clone)]
+struct Spot {
+    pub height: u32,
+    pub visited: bool,
+}
+
+struct Heightmap(Vec<Vec<Spot>>);
 
 impl Heightmap {
     pub fn new(width: usize, height: usize, input: &str) -> Heightmap {
-        let mut hm = Heightmap(vec![vec![9; width + 2]; height + 2]);
+        let mut hm = Heightmap(vec![
+            vec![
+                Spot {
+                    height: 9,
+                    visited: false
+                };
+                width + 2
+            ];
+            height + 2
+        ]);
         input.split("\n").enumerate().for_each(|(i, l)| {
             l.trim().chars().enumerate().for_each(|(j, c)| {
-                hm.0[i + 1][j + 1] = c.to_digit(10).unwrap();
+                hm.0[i + 1][j + 1].height = c.to_digit(10).unwrap();
             })
         });
         hm
@@ -26,11 +41,11 @@ impl Heightmap {
         let x = x + 1;
         let y = y + 1;
 
-        let h = self.0[x][y];
-        if self.0[x - 1][y] > h
-            && self.0[x + 1][y] > h
-            && self.0[x][y - 1] > h
-            && self.0[x][y + 1] > h
+        let h = self.0[x][y].height;
+        if self.0[x - 1][y].height > h
+            && self.0[x + 1][y].height > h
+            && self.0[x][y - 1].height > h
+            && self.0[x][y + 1].height > h
         {
             1 + h
         } else {
