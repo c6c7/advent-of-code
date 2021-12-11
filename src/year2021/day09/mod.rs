7daea863ec44e5f6779d3ac3cc882346d2256505
Charles Celerier <charles@chckyn.org>
2021-12-11 13:37:54 -0800

    [2021] Solved Day 9 Part 2

--- src/year2021/day09/mod.rs
+++ src/year2021/day09/mod.rs
@@ -1,3 +1,5 @@
+use std::collections::BinaryHeap;
+
 const INPUT_WIDTH: usize = 100;
 const INPUT_HEIGHT: usize = 100;
 
@@ -9,29 +11,65 @@ pub fn part1(input: String) {
     println!("Answer: {}", low_point_risk_level_sum);
 }
 
+pub fn part2(input: String) {
+    let mut heightmap = Heightmap::new(INPUT_WIDTH, INPUT_HEIGHT, &input);
+    let mut basins = heightmap.basin_label();
+    let mut answer = 1;
+    println!("Basin 1: {:?}", {
+        let b = basins.pop().unwrap();
+        answer *= b.size;
+        b
+    });
+    println!("Basin 2: {:?}", {
+        let b = basins.pop().unwrap();
+        answer *= b.size;
+        b
+    });
+    println!("Basin 3: {:?}", {
+        let b = basins.pop().unwrap();
+        answer *= b.size;
+        b
+    });
+    println!("Answer: {}", answer);
+}
+
 #[derive(Clone)]
 struct Spot {
     pub height: u32,
     pub visited: bool,
+    pub basin_index: i64,
 }
 
-struct Heightmap(Vec<Vec<Spot>>);
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct Basin {
+    pub size: usize,
+    pub seed: (usize, usize),
+}
+
+struct Heightmap {
+    map: Vec<Vec<Spot>>,
+    basins: Vec<Basin>,
+}
 
 impl Heightmap {
     pub fn new(width: usize, height: usize, input: &str) -> Heightmap {
-        let mut hm = Heightmap(vec![
-            vec![
-                Spot {
-                    height: 9,
-                    visited: false
-                };
-                width + 2
-            ];
-            height + 2
-        ]);
+        let mut hm = Heightmap {
+            map: vec![
+                vec![
+                    Spot {
+                        height: 9,
+                        visited: false,
+                        basin_index: -1,
+                    };
+                    width + 2
+                ];
+                height + 2
+            ],
+            basins: vec![],
+        };
         input.split("\n").enumerate().for_each(|(i, l)| {
             l.trim().chars().enumerate().for_each(|(j, c)| {
-                hm.0[i + 1][j + 1].height = c.to_digit(10).unwrap();
+                hm.map[i + 1][j + 1].height = c.to_digit(10).unwrap();
             })
         });
         hm
@@ -41,15 +79,55 @@ impl Heightmap {
         let x = x + 1;
         let y = y + 1;
 
-        let h = self.0[x][y].height;
-        if self.0[x - 1][y].height > h
-            && self.0[x + 1][y].height > h
-            && self.0[x][y - 1].height > h
-            && self.0[x][y + 1].height > h
+        let h = self.map[x][y].height;
+        if self.map[x - 1][y].height > h
+            && self.map[x + 1][y].height > h
+            && self.map[x][y - 1].height > h
+            && self.map[x][y + 1].height > h
         {
             1 + h
         } else {
             0
         }
+    }
+
+    fn basin_label<'a>(&'a mut self) -> BinaryHeap<&'a Basin> {
+        for i in 0..self.map.len() - 2 {
+            for j in 0..self.map[i].len() - 2 {
+                let i = i + 1;
+                let j = j + 1;
+
+                if self.map[i][j].height == 9 || self.map[i][j].visited {
+                    continue;
+                }
+                let mut basin = Basin {
+                    seed: (i - 1, j - 1),
+                    size: 0,
+                };
+                let basin_index = self.basins.len() as i64;
+
+                self.map[i][j].visited = true;
+                let mut queue = vec![(i, j)];
+                while !queue.is_empty() {
+                    let (x, y) = queue.pop().unwrap();
+                    self.map[x][y].basin_index = basin_index;
+                    basin.size += 1;
+
+                    vec![(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+                        .into_iter()
+                        .for_each(|(a, b)| {
+                            if !(self.map[a][b].visited || self.map[a][b].height == 9) {
+                                self.map[a][b].visited = true;
+                                queue.push((a, b));
+                            }
+                        });
+                }
+                self.basins.push(basin);
+            }
+        }
+        self.basins.iter().fold(BinaryHeap::new(), |mut heap, b| {
+            heap.push(b);
+            heap
+        })
     }
 }
