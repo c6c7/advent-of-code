a1e8066b67a99dda9087a43dec3dd07dd0a1610e
Charles Celerier <charles@chckyn.org>
2021-12-11 23:14:29 -0800

    [2021] Refactor Day 10 Part 1 to extend for Part 2

--- src/year2021/day10/mod.rs
+++ src/year2021/day10/mod.rs
@@ -1,35 +1,38 @@
+enum ParseResult {
+    Incomplete(Vec<char>),
+    SyntaxError(u32),
+}
+
+fn parse_chunks(chunks: &str) -> ParseResult {
+    let mut open_chunks = vec![];
+
+    macro_rules! syntax_error_check {
+        ($open:literal, $score:literal) => {
+            if let Some($open) = open_chunks.pop() {
+            } else {
+                return ParseResult::SyntaxError($score);
+            }
+        };
+    }
+
+    for c in chunks.chars() {
+        match c {
+            '(' | '[' | '{' | '<' => open_chunks.push(c),
+            ')' => syntax_error_check!('(', 3),
+            ']' => syntax_error_check!('[', 57),
+            '}' => syntax_error_check!('{', 1197),
+            '>' => syntax_error_check!('<', 25137),
+            _ => unreachable!(),
+        }
+    }
+
+    ParseResult::Incomplete(open_chunks)
+}
+
 pub fn part1(input: String) {
     let score = input.split("\n").fold(0, |score_acc, l| {
-        let mut open_chunks = vec![];
-        for c in l.chars() {
-            match c {
-                '(' | '[' | '{' | '<' => open_chunks.push(c),
-                ')' => {
-                    if let Some('(') = open_chunks.pop() {
-                    } else {
-                        return score_acc + 3;
-                    }
-                }
-                ']' => {
-                    if let Some('[') = open_chunks.pop() {
-                    } else {
-                        return score_acc + 57;
-                    }
-                }
-                '}' => {
-                    if let Some('{') = open_chunks.pop() {
-                    } else {
-                        return score_acc + 1197;
-                    }
-                }
-                '>' => {
-                    if let Some('<') = open_chunks.pop() {
-                    } else {
-                        return score_acc + 25137;
-                    }
-                }
-                _ => unreachable!(),
-            }
+        if let ParseResult::SyntaxError(score) = parse_chunks(l) {
+            return score_acc + score;
         }
         score_acc
     });
