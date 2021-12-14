acdb74c9261e0dac152064cc67348628704b5574
Charles Celerier <charles@chckyn.org>
2021-12-13 21:42:17 -0800

    [2021] Solved Day 12 Part 1

--- src/year2021/day12/mod.rs
+++ src/year2021/day12/mod.rs
@@ -4,7 +4,30 @@ type Cave<'a> = &'a str;
 type CaveMap<'a> = HashMap<Cave<'a>, (bool, Vec<Cave<'a>>)>;
 type CavePath<'a> = Vec<Cave<'a>>;
 
-pub fn part1(input: String) {}
+pub fn part1(input: String) {
+    let mut cave_map = CaveMap::new();
+    input.trim().split("\n").for_each(|l| {
+        let mut parts = l.trim().split("-");
+        let left = parts.next().unwrap();
+        let right = parts.next().unwrap();
+        insert_edge(&mut cave_map, left, right);
+        insert_edge(&mut cave_map, right, left);
+    });
+
+    let mut all_paths = vec![];
+    let mut root_path = vec![];
+    follow(&mut all_paths, &mut cave_map, &mut root_path, "start");
+
+    println!("Number of paths: {}", all_paths.len());
+}
+
+fn insert_edge<'a>(cave_map: &mut CaveMap<'a>, from: Cave<'a>, to: Cave<'a>) {
+    if let Some((_, neighbors)) = cave_map.get_mut(from) {
+        neighbors.push(to);
+    } else {
+        cave_map.insert(from, (false, vec![to]));
+    }
+}
 
 fn follow<'a>(
     all_paths: &mut Vec<CavePath<'a>>,
