3eddb433d75946fb9c5830a34482e89d8a5ab88d
Charles Celerier <charles@chckyn.org>
2021-12-13 22:58:20 -0800

    [2021] Solved Day 12 Part 1...again

--- src/year2021/day12/mod.rs
+++ src/year2021/day12/mod.rs
@@ -1,12 +1,28 @@
 use std::collections::{HashMap, HashSet};
 
 type Cave<'a> = &'a str;
-type CaveMap<'a> = HashMap<Cave<'a>, (usize, Vec<Cave<'a>>)>;
+type CaveMap<'a> = HashMap<Cave<'a>, Vec<Cave<'a>>>;
 type CavePath<'a> = Vec<Cave<'a>>;
+type SmallCaveSet<'a> = HashSet<Cave<'a>>;
 
 pub fn part1(input: String) {
+    let (mut cave_map, mut small_caves) = parse_input(&input);
+    let mut all_paths = HashSet::new();
+    let mut root_path = vec![];
+    follow(
+        &mut all_paths,
+        &mut small_caves,
+        &mut cave_map,
+        &mut root_path,
+        "start",
+        true,
+    );
+    println!("Number of paths: {}", all_paths.len());
+}
+
+fn parse_input(input: &str) -> (CaveMap, SmallCaveSet) {
     let mut cave_map = CaveMap::new();
-    let mut small_caves = HashSet::new();
+    let mut small_caves = SmallCaveSet::new();
     input.trim().split("\n").for_each(|l| {
         let mut parts = l.trim().split("-");
         let left = parts.next().unwrap();
@@ -17,66 +33,57 @@ pub fn part1(input: String) {
         if right.clone().to_lowercase() == right {
             small_caves.insert(right);
         }
-        insert_edge(&mut cave_map, left, right);
-        insert_edge(&mut cave_map, right, left);
-    });
-
-    let mut all_paths = HashSet::new();
-
-    for small_cave in small_caves {
-        if small_cave == "start" || small_cave == "end" {
-            continue;
+        if left != "end" && right != "start" {
+            insert_edge(&mut cave_map, left, right);
         }
-        let (visits_remaining, _) = cave_map.get_mut(small_cave).unwrap();
-        *visits_remaining = 2;
-
-        let mut root_path = vec![];
-        follow(&mut all_paths, &mut cave_map, &mut root_path, "start");
-
-        let (visits_remaining, _) = cave_map.get_mut(small_cave).unwrap();
-        *visits_remaining = 1;
-    }
-
-    println!("Number of paths: {}", all_paths.len());
+        if right != "end" && left != "start" {
+            insert_edge(&mut cave_map, right, left);
+        }
+    });
+    (cave_map, small_caves)
 }
 
 fn insert_edge<'a>(cave_map: &mut CaveMap<'a>, from: Cave<'a>, to: Cave<'a>) {
-    if let Some((_, neighbors)) = cave_map.get_mut(from) {
+    if let Some(neighbors) = cave_map.get_mut(from) {
         neighbors.push(to);
     } else {
-        cave_map.insert(from, (1, vec![to]));
+        cave_map.insert(from, vec![to]);
     }
 }
 
 fn follow<'a>(
     all_paths: &mut HashSet<CavePath<'a>>,
+    small_caves: &mut HashSet<Cave<'a>>,
     map: &mut CaveMap<'a>,
     path: &mut Vec<Cave<'a>>,
     to: Cave<'a>,
+    double_visit_used: bool,
 ) {
+    let mut next_double_visit_used = double_visit_used;
     path.push(to);
     if to == "end" {
         all_paths.insert(path.clone());
+        return;
     }
 
-    if to.clone().to_uppercase() != to {
-        let (visits_remaining, _) = map.get_mut(to).unwrap();
-        *visits_remaining -= 1;
+    if to.clone().to_lowercase() == to {
+        if !small_caves.remove(to) {
+            if double_visit_used {
+                return;
+            }
+            next_double_visit_used = true;
+        }
     }
 
     let original_length = path.len();
-    let neighbors = map.get(to).unwrap().1.clone();
+    let neighbors = map.get(to).unwrap().clone();
     for c in neighbors {
-        if map.get(c).unwrap().0 == 0 {
-            continue;
-        }
-        follow(all_paths, map, path, c);
+        follow(all_paths, small_caves, map, path, c, next_double_visit_used);
         path.truncate(original_length);
     }
 
-    if to.clone().to_uppercase() != to {
-        let (visits_remaining, _) = map.get_mut(to).unwrap();
-        *visits_remaining += 1;
+    if to.clone().to_lowercase() == to && next_double_visit_used == double_visit_used {
+        small_caves.insert(to);
     }
 }
 
