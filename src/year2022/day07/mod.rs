5bb9fe4de84b44caf9dc044a7e096bd5d0af654e
c6c7 <public-dev@chcl.io>
2022-12-07 20:42:49 +0000

    [2022] Day 7: Add ability to add directories and files

--- src/year2022/day07/mod.rs
+++ src/year2022/day07/mod.rs
@@ -1,6 +1,6 @@
 use std::{
     cell::RefCell,
-    collections::HashSet,
+    collections::{HashMap, HashSet},
     fmt,
     hash::{Hash, Hasher},
     rc::Rc,
@@ -9,8 +9,8 @@ use std::{
 struct Directory {
     parent: Option<Rc<RefCell<Directory>>>,
     name: String,
-    directory_children: HashSet<Rc<RefCell<Directory>>>,
-    file_children: HashSet<File>,
+    directory_children: HashMap<String, Rc<RefCell<Directory>>>,
+    file_children: HashMap<String, File>,
 }
 
 impl fmt::Debug for Directory {
@@ -19,18 +19,22 @@ impl fmt::Debug for Directory {
             None => "<none>".to_string(),
             Some(p) => p.as_ref().borrow().name.clone(),
         };
+        let directory_children = self
+            .directory_children
+            .iter()
+            .map(|(name, _)| name.clone())
+            .collect::<HashSet<_>>();
+
+        let file_children = self
+            .file_children
+            .iter()
+            .map(|(name, _)| name.clone())
+            .collect::<HashSet<_>>();
         f.debug_struct("Directory")
             .field("parent", &parent_name)
             .field("name", &self.name)
-            .field(
-                "directory_children",
-                &self
-                    .directory_children
-                    .iter()
-                    .map(|d| d.as_ref().borrow().name.clone())
-                    .collect::<HashSet<_>>(),
-            )
-            .field("file_children", &self.file_children)
+            .field("directory_children", &directory_children)
+            .field("file_children", &file_children)
             .finish()
     }
 }
@@ -41,32 +45,38 @@ enum FsObject {
 }
 
 impl Directory {
-    fn new_root() -> Self {
-        Directory {
-            parent: None,
-            name: "".to_string(),
-            directory_children: HashSet::new(),
-            file_children: HashSet::new(),
+    fn new(parent: Option<Rc<RefCell<Directory>>>, name: &str) -> Rc<RefCell<Directory>> {
+        let child = Rc::new(RefCell::new(Directory {
+            parent,
+            name: String::from(name),
+            directory_children: HashMap::new(),
+            file_children: HashMap::new(),
+        }));
+        if let Some(parent) = &child.as_ref().borrow().parent {
+            parent
+                .as_ref()
+                .borrow_mut()
+                .directory_children
+                .insert(child.as_ref().borrow().name.clone(), Rc::clone(&child));
         }
+        child
+    }
+
+    fn add_child_file(parent: Rc<RefCell<Self>>, mut child: File) {
+        child.parent.replace(Rc::clone(&parent));
+        parent
+            .as_ref()
+            .borrow_mut()
+            .file_children
+            .insert(child.name.clone(), child);
     }
 }
 
-impl Hash for Directory {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.name.hash(state);
-    }
-}
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct File {
+    parent: Option<Rc<RefCell<Directory>>>,
     name: String,
     size: usize,
-}
-
-impl Hash for File {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.name.hash(state);
-    }
 }
 
 pub fn part1(input: String) {}
@@ -80,11 +90,60 @@ mod tests {
     #[test]
     #[traced_test]
     fn simple_directory_construction() {
-        let root = Directory::new_root();
-        debug!("{root:?}");
+        let root = Directory::new(None, "root");
+        debug!("{:?}", root.as_ref().borrow());
     }
 
     #[test]
     #[traced_test]
-    fn hand_constructed_fs() {}
+    fn single_file_fs() {
+        let f = File {
+            parent: None,
+            name: "a".to_string(),
+            size: 0,
+        };
+        let root = Directory::new(None, "root");
+        Directory::add_child_file(root.clone(), f);
+        assert_eq!(1, root.as_ref().borrow().file_children.len());
+
+        debug!("{:?}", root.as_ref().borrow());
+    }
+
+    #[test]
+    #[traced_test]
+    fn nested_file_fs() {
+        let f = File {
+            parent: None,
+            name: "a".to_string(),
+            size: 0,
+        };
+        let root = Directory::new(None, "root");
+        let sub_root = Directory::new(Some(root.clone()), "sub_root");
+        assert_eq!(0, root.as_ref().borrow().file_children.len());
+        assert_eq!(1, root.as_ref().borrow().directory_children.len());
+
+        Directory::add_child_file(sub_root.clone(), f);
+        assert_eq!(1, sub_root.as_ref().borrow().file_children.len());
+        assert_eq!(0, sub_root.as_ref().borrow().directory_children.len());
+
+        debug!("root: {:?}", root.as_ref().borrow());
+        debug!("sub_root: {:?}", sub_root.as_ref().borrow());
+    }
+
+    #[test]
+    #[traced_test]
+    fn duplicate_named_file_dropped() {
+        let f = File {
+            parent: None,
+            name: "a".to_string(),
+            size: 0,
+        };
+        let root = Directory::new(None, "root");
+        Directory::add_child_file(root.clone(), f.clone());
+        assert_eq!(1, root.as_ref().borrow().file_children.len());
+        Directory::add_child_file(root.clone(), f.clone());
+        assert_eq!(1, root.as_ref().borrow().file_children.len());
+
+        debug!("root: {:?}", root.as_ref().borrow());
+    }
 }
