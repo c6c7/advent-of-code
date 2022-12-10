3b7bcc0ee74e6536cab3998acffc1694da9ff4a0
c6c7 <public-dev@chcl.io>
2022-12-10 18:05:51 +0000

    [2022][10] Solved Part 2!

--- src/year2022/day10/mod.rs
+++ src/year2022/day10/mod.rs
@@ -1,8 +1,6 @@
-use std::convert::TryInto;
-
 #[derive(Debug)]
 struct CpuState {
-    cycles_completed: usize,
+    cycles_completed: i64,
     signal_strength: i64,
     accumulated_signal_strength: i64,
     register_x: i64,
@@ -11,12 +9,55 @@ struct CpuState {
 impl CpuState {
     pub fn advance_cycle(&mut self) {
         self.cycles_completed += 1;
-        self.signal_strength =
-            self.register_x * TryInto::<i64>::try_into(self.cycles_completed).unwrap();
+        self.signal_strength = self.register_x * self.cycles_completed;
         if self.cycles_completed % 40 == 20 {
             self.accumulated_signal_strength += self.signal_strength;
             tracing::debug!("{self:?}");
         }
+    }
+}
+
+struct Sprite(i64);
+
+struct Crt {
+    display: [[char; 40]; 6],
+    cpu: CpuState,
+    sprite: Sprite,
+}
+
+impl Crt {
+    pub fn render(&mut self) {
+        let pixel = &mut self.display[(self.cpu.cycles_completed / 40) as usize]
+            [(self.cpu.cycles_completed % 40) as usize];
+        let cursor = self.cpu.cycles_completed % 40;
+        if self.sprite.0 - 1 <= cursor && cursor <= self.sprite.0 + 1 {
+            *pixel = '#';
+        } else {
+            *pixel = '.';
+        }
+    }
+
+    pub fn noop(&mut self) {
+        self.render();
+        self.cpu.advance_cycle();
+    }
+    pub fn addx(&mut self, inc: i64) {
+        self.render();
+        self.cpu.advance_cycle();
+        self.render();
+        self.cpu.advance_cycle();
+        self.cpu.register_x += inc;
+        self.sprite.0 = self.cpu.register_x;
+    }
+}
+
+impl std::fmt::Display for Crt {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, " {}", ['-'; 39].iter().collect::<String>())?;
+        for row in self.display {
+            writeln!(f, "|{}|", row.iter().collect::<String>())?;
+        }
+        writeln!(f, " {}", ['-'; 39].iter().collect::<String>())
     }
 }
 
@@ -55,4 +96,40 @@ pub fn part1(input: String) {
     tracing::info!("Part 1 Answer: {}", cpu_state.accumulated_signal_strength);
 }
 
-pub fn part2(_input: String) {}
+pub fn part2(input: String) {
+    let crt = input
+        .trim()
+        .split('\n')
+        .try_fold(
+            Crt {
+                cpu: CpuState {
+                    cycles_completed: 0,
+                    signal_strength: 0,
+                    accumulated_signal_strength: 0,
+                    register_x: 1,
+                },
+                display: [['-'; 40]; 6],
+                sprite: Sprite(1),
+            },
+            |mut crt, line| {
+                match &line.as_bytes()[..4] {
+                    b"noop" => {
+                        crt.noop();
+                    }
+                    b"addx" => {
+                        crt.addx(
+                            String::from_utf8(line.as_bytes()[5..].to_vec())
+                                .unwrap()
+                                .parse::<i64>()
+                                .unwrap(),
+                        );
+                    }
+                    _ => unreachable!(),
+                }
+                Ok::<Crt, ()>(crt)
+            },
+        )
+        .unwrap();
+
+    tracing::info!("Part 2 Answer:\n{}", crt);
+}
